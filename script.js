e0a17650c305aed2188bba8bf9d18f2c36b63ed9
xspazioxspaziox <xspazioxspaziox@gmail.com>
2025-09-21 23:50:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+async function includeHTML(id, file) {
+  try {
+    const res = await fetch(file);
+    if (!res.ok) throw new Error(`Impossibile caricare ${file}`);
+    const text = await res.text();
+    document.getElementById(id).innerHTML = text;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  includeHTML("head-content", "head.html");
+  includeHTML("header", "header.html");
+  includeHTML("footer", "footer.html");
+});
