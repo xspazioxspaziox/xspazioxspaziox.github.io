d42177dcc41ee68ad3005156388b3864d4e0072f
xspazioxspaziox <xspazioxspaziox@gmail.com>
2025-10-07 23:35:07 +0200

    Update hydra.js

--- assets/js/hydra.js
+++ assets/js/hydra.js
@@ -10,4 +10,11 @@ if (hydraCanvas) {
 
   // Patch visual Hydra (puoi modificarla liberamente)
   osc(4, 0.1, 1.2)
-
+      .modulateScale(osc(8).rotate(Math.sin(time)), .5)
+      .thresh(.8)
+.colorama(5)
+.modulateScale(o0)
+      .diff(src(o0).scale(1.8))
+      .modulateScale(osc(2).modulateRotate(o0, .74))
+      .diff(src(o0).rotate([-.012, .01, -.002, 0]).scrollY(0, [-1/199800, 0].fast(0.7)))
+      .out();
