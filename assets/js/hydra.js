fd280660795d52d00a1583fa4e97a44e1f987748
xspazioxspaziox <xspazioxspaziox@gmail.com>
2025-10-13 23:06:54 +0200

    Update hydra.js

--- assets/js/hydra.js
+++ assets/js/hydra.js
@@ -1,9 +1,9 @@
-    osc(4, 0.1, 1.2)
-      .modulateScale(osc(8).rotate(Math.sin(time)), .5)
-      .thresh(.8)
-.colorama(5)
+  osc(10, 0.08, 1)
+      .modulateScale(osc(6).rotate(Math.sin(time)), .5)
+      .thresh(.4)
+.colorama(50)
 .modulateScale(o0)
-      .diff(src(o0).scale(1.8))
-      .modulateScale(osc(2).modulateRotate(o0, .74))
-      .diff(src(o0).rotate([-.012, .01, -.002, 0]).scrollY(0, [-1/199800, 0].fast(0.7)))
-      .out();
+      .diff(src(o0).scale(3))
+      .modulateScale(osc(1).modulateRotate(o0, .7))
+      .diff(src(o0).rotate([-.02, .01, -.002, 0]).scrollY(0, [-0.006, 0].fast(0.7)))
+      .out(o0);
