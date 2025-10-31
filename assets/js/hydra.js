c41b60f7791c3e5fe2490271aec8f28b7875a113
xspazioxspaziox <xspazioxspaziox@gmail.com>
2025-10-31 16:01:39 +0100

    Delete assets directory

--- assets/js/hydra.js
+++
@@ -1,9 +0,0 @@
-  osc(10, 0.08, 1)
-      .modulateScale(osc(6).rotate(Math.sin(time)), .5)
-      .thresh(.4)
-.colorama(50)
-.modulateScale(o0)
-      .diff(src(o0).scale(3))
-      .modulateScale(osc(1).modulateRotate(o0, .7))
-      .diff(src(o0).rotate([-.02, .01, -.002, 0]).scrollY(0, [-0.006, 0].fast(0.7)))
-      .out(o0);
--- assets/sfx/readme.md
+++
@@ -1,7 +0,0 @@
-Crea questi file audio nel repo:
-assets/
- └── sfx/
-      ├── download.mp3
-      ├── eventi.mp3
-      ├── archivio.mp3
-      └── random.mp3
